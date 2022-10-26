89e4a146aff040f85eaebdf9f7651c61dab092f6
dmitrii <dimaaasik.s@gmail.com>
2022-10-27 00:55:34 +0600

    Added style with bootstrap. Some js code refactoring.

--- frontend/src/app/training/selector/training-selector.component.ts
+++ frontend/src/app/training/selector/training-selector.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
+import { Observable, of } from 'rxjs';
 import { TrainingProvider } from '../training-provider.service'
 
 import { environment } from '../../../environments/environment';
@@ -14,54 +14,64 @@ import { environment } from '../../../environments/environment';
 })
 export class TrainingSelectorComponent implements OnInit {
 
-  private API_WORD_ORIGINS_URL = 'api/v1/word/tags';
   private API_URL = environment.API_URL;
+  private API_WORD_TAGS_URL = 'api/v1/word/tags';
 
-  // tags: string;
-  // allTags: string;
   tagsArray: string[];
   selectedTag: string;
   allTypes = ["TAGGED", "REPEAT", "RANDOM"];
   selectedType: string;
-  // type: string;
   size: number;
 
   constructor(
     private trainingProvider: TrainingProvider,
-    private location: Location,
     private router: Router,
     private http: HttpClient
   ) {
-    // this.type = '';
-    // this.tags = '';
-    // this.allTags = '';
     this.selectedTag = '';
     this.selectedType = '';
     this.size = 0;
     this.tagsArray = new Array();
 
     this.http.get<string[]>(
-      `${this.API_URL}/${this.API_WORD_ORIGINS_URL}`
-    ).subscribe(res => {
-      this.tagsArray.push(...res);
-    })
+      `${this.API_URL}/${this.API_WORD_TAGS_URL}`
+    ).subscribe(tags => 
+      {
+        this.tagsArray.push(...tags);
+        this.selectedTag = tags[0];
+      }, 
+      this.handleError
+    )
   }
 
   ngOnInit(): void {
   }
 
   start(): void {
-    // let tagsArray = this.tags.split(',');
     this.trainingProvider.loadTraining([this.selectedTag], this.selectedType, this.size)
       .then(res => this.router.navigate(['/process']));
   }
 
   clear(): void {
-    // this.type = '';
-    // this.tags = '';
-    // this.allTags = '';
     this.size = 0;
     this.selectedType = '';
     this.tagsArray = new Array();
   }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+     private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+  
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+  
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
 }
